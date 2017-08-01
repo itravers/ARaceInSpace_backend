3c821ed05fa20e6e06185a3d4cf6e60e99330fb0
Isaac Assegai <Isaac.A.Travers@gmail.com>
2017-08-01 13:40:35 -0700

    Checking times before adding new records works
    Signed-off-by: Isaac Assegai <Isaac.A.Travers@gmail.com>

--- routes/leaderboards.js
+++ routes/leaderboards.js
@@ -24,26 +24,60 @@ router.get('/json', function(req, res, next){
 router.get('/update/:level/:place/:name/:time/:id', function(req, res, next){
 	var db = req.db;
 	var collection = db.get('leaderboards');
-	/*collection.find(
-		{},
-		{},
-		function(e, docs){
-			res.send(docs);
-		}
-	);*/
-/*	collection.update( 
-		{ data: { $elemMatch: { place: "1st" } } },
-		{ $set: { 'data.$.name': req.params.name } },
-		function(e, docs){
-			res.send(docs);	
-		}
-	 );
-*/
-
         var place = req.params.place-1;
-        var q = "levels.$.data."+place+".name";
+	var newTime = parseInt(req.params.time);
+	var level = req.params.level-1;//since leves are 0 indexed in db
+        var nameq = "levels."+level+".data."+place+".name";
+	var idq =   "levels."+level+".data."+place+".id";
+	var timeq = "levels."+level+".data."+place+".time";
+	//var timeq = "levels.$.data."+place+".time";
+	var levelName = "Level "+req.params.level;
 	var obj = {};
-	obj[q] = req.params.name;
+	obj[nameq] = req.params.name;
+	obj[timeq] = req.params.time;
+	obj[idq] = req.params.id;
+
+
+
+	collection.find(
+		{},
+		{},
+		function(e, docs){
+			if(e==null){
+				var previousTime = docs[0].levels[level].data[place].time;
+				previousTime = parseInt(previousTime);
+				var obj = {};
+        obj[nameq] = req.params.name;
+        obj[timeq] = req.params.time;
+        obj[idq] = req.params.id;
+
+				console.log("newTime: " + newTime + " previousTime: " + previousTime); 
+				if(newTime < previousTime){//the new time is less than the previous time, put in db
+				
+					 collection.update(
+						{},
+				                //{ levels: { $elemMatch: {name : req.params.level}} },
+              					  { $set: obj},
+              					  function(e, docs){
+                				        if(e == null){
+                        				        res.send(docs);
+                       					 }else{
+                        				        res.send(e);
+                       					 }
+               					  }
+      					);
+
+
+				}else{
+					res.send("your time of " +newTime+" did not beat "+ previousTime);
+				}
+			}else{
+				res.send(e);
+			}
+		}
+	);
+/*
+
 	collection.update(
                 { levels: { $elemMatch: {name : req.params.level}} },
                 //{ $set: { "levels.$.data.place.name": req.params.name } },
@@ -58,7 +92,7 @@ router.get('/update/:level/:place/:name/:time/:id', function(req, res, next){
          );
 
 
-	
+*/	
 });
 
 
